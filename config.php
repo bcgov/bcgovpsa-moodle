3311332f24a27611e6cc66d32d38e49f16366ab0
Allan Haggett <allan.haggett@gov.bc.ca>
2023-09-29 03:02:11 +0000

    Spacing

--- config.php
+++ config.php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * BCGovPSA config.
+ * BCGovPSA config. 
  *
  * @package   theme_bcgovpsa
  * @copyright 2023 Allan.Haggett@gov.bc.ca
